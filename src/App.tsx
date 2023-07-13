8e412724b47198bb6a9717cd0c072270cd575b13
leoziondev <leozionsuporte@gmail.com>
2023-07-13 08:36:23 -0300

    create and config store

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Provider as ReduxProvider } from 'react-redux';
+
+import { AddTodo } from "./components/AddTodo";
+import { TodoList } from "./components/TodoList";
+import { store } from './store';
+
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <ReduxProvider store={store}>
+      <TodoList />
+      <AddTodo />
+    </ReduxProvider>
   )
 }
