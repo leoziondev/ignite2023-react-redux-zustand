e78be35e57b4466ed190c9abffbaac61cb9a8f80
leoziondev <leozionsuporte@gmail.com>
2023-07-17 11:38:36 -0300

    modules and player UI

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,4 +1,5 @@
-import { MessageCircle } from 'lucide-react'
+import { ChevronDown, MessageCircle, Video } from 'lucide-react'
+import ReactPlayer from 'react-player'
 
 export function Player() {
     return (
@@ -17,11 +18,56 @@ export function Player() {
                     </button>
                 </div>
 
-                <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
+                <main className="relative flex flex-col md:flex-row overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
                     <div className="flex-1">
-                        video
+                        <div className="w-full bg-zinc-950 aspect-video">
+                            <ReactPlayer
+                                width="100%"
+                                height="100%"
+                                constrols
+                                url="https://www.youtube.com/watch?v=Jai8w6K_GnY"
+                            />
+                        </div>
                     </div>
-                    <aside className="w-80 border-l border-zinc-800 bg-zinc-900 h-[600px]"></aside>
+                    <aside className="w-full md:w-80 border-l border-zinc-800 bg-zinc-900">
+                        <div>
+                            <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+                                <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
+                                    1
+                                </div>
+
+                                <div className="flex flex-col gap-1 text-left">
+                                    <strong className="text-sm">Desvendadndo o Redux</strong>
+                                    <span className="text-xs text-zinc-400">12 aulas</span>
+                                </div>
+
+                                <ChevronDown className="h-4 w-4 ml-auto text-zinc-400" />
+                            </button>
+
+                            <nav className="relative flex flex-col gap-4 p-6">
+                                <button className="flex items-center gap-3 text-sm text-zinc-400">
+                                    <Video className="h-4 w-4 text-zinc-500" />
+                                    <span className="">Fundamentos do Redux</span>
+                                    <span className="ml-auto font-mono text-xs text-zinc-500">09:13</span>
+                                </button>
+                                <button className="flex items-center gap-3 text-sm text-zinc-400">
+                                    <Video className="h-4 w-4 text-zinc-500" />
+                                    <span className="">Fundamentos do Redux</span>
+                                    <span className="ml-auto font-mono text-xs text-zinc-500">09:13</span>
+                                </button>
+                                <button className="flex items-center gap-3 text-sm text-zinc-400">
+                                    <Video className="h-4 w-4 text-zinc-500" />
+                                    <span className="">Fundamentos do Redux</span>
+                                    <span className="ml-auto font-mono text-xs text-zinc-500">09:13</span>
+                                </button>
+                                <button className="flex items-center gap-3 text-sm text-zinc-400">
+                                    <Video className="h-4 w-4 text-zinc-500" />
+                                    <span className="">Fundamentos do Redux</span>
+                                    <span className="ml-auto font-mono text-xs text-zinc-500">09:13</span>
+                                </button>
+                            </nav>
+                        </div>
+                    </aside>
                 </main>
             </div>
         </div>
