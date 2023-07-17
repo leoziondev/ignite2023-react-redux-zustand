322b64290998d8d0db07880c900148d8b91a098b
leoziondev <leozionsuporte@gmail.com>
2023-07-17 10:46:24 -0300

    fix postcss config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.tsx'
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
