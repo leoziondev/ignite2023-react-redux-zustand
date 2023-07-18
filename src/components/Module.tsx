aa968dc9c7185d9cfd5abba434291f97278f566f
leoziondev <leozionsuporte@gmail.com>
2023-07-17 21:58:46 -0300

    selecting current lesson video player

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,9 +1,11 @@
+import { useDispatch } from "react-redux";
 import { useAppSelector } from "../store";
 import { Lesson } from "./Lesson";
 
 import * as Collapsible from "@radix-ui/react-collapsible";
 
 import { ChevronDown } from "lucide-react";
+import { play } from "../store/slices/player";
 
 interface ModuleProps {
     moduleIndex: number
@@ -12,6 +14,7 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+    const dispatch = useDispatch()
     const lessons = useAppSelector(state => {
         return state.player.course.modules[moduleIndex].lessons
     })
@@ -33,12 +36,13 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
             <Collapsible.Content>
                 <nav className="relative flex flex-col gap-4 p-6">
-                    {lessons.map(lesson => {
+                    {lessons.map((lesson, lessonIndex) => {
                         return (
                             <Lesson
                                 key={lesson.id}
                                 title={lesson.title}
                                 duration={lesson.duration}
+                                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
                             />
                         )
                     })}            
