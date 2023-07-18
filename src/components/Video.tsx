c8a4010ec931b0b2246f62704016491abbbe965c
leoziondev <leozionsuporte@gmail.com>
2023-07-18 10:55:36 -0300

    create global hook

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,18 +1,12 @@
 import ReactPlayer from 'react-player'
 import { useAppSelector } from '../store'
 import { useDispatch } from 'react-redux'
-import { next } from '../store/slices/player'
+import { next, useCurrentLesson } from '../store/slices/player'
 
 export function Video() {
     const dispatch = useDispatch()
 
-    const lesson = useAppSelector(state => {
-        const { currentModuleIndex, currentLessonIndex } = state.player
-
-        const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-
-        return currentLesson
-    })
+    const { currentLesson } = useCurrentLesson()
 
     function handlePlayNext() {
         dispatch(next())
@@ -26,7 +20,7 @@ export function Video() {
                 controls
                 playing
                 onEnded={handlePlayNext}
-                url={`https://www.youtube.com/watch?v=${lesson.id}`}
+                url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
             />
         </div>
     )
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,15 +1,23 @@
+import { useEffect } from 'react'
 import { Header } from '../components/Header'
 import { Video } from '../components/Video'
 import { Module } from '../components/Module'
 
 import { MessageCircle } from 'lucide-react'
 import { useAppSelector } from '../store'
+import { useCurrentLesson } from '../store/slices/player'
 
 
 export function Player() {
     const modules = useAppSelector(state => {
         return state.player.course.modules
     })
+
+    const { currentLesson } = useCurrentLesson()
+
+    useEffect(() => {
+        document.title = `Assistindo: ${currentLesson.title}`
+    }, [currentLesson])
 
     return (
         <div className="h-screen bg-zinc-950 text-zinc-50 flex items-center justify-center">
